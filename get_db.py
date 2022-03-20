72d73298cc4ce0de1c7a6fc00da2239ac1b5ca3d
cgraaaj <cgraaaj@gmail.com>
2022-03-20 13:50:21 +0530

    added .env logic

--- get_db.py
+++ get_db.py
@@ -1,8 +1,15 @@
+import os
+
 from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_database():
+
+    MONGO_PASS = os.environ.get("MONGO_PASS")
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb+srv://velan_mongodb:Raju6713@cluster0.64cqd.mongodb.net/tradeStrategies?retryWrites=true&w=majority"
+    CONNECTION_STRING = f"mongodb+srv://velan_mongodb:{MONGO_PASS}@cluster0.64cqd.mongodb.net/tradeStrategies?retryWrites=true&w=majority"
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     client = MongoClient(CONNECTION_STRING)
